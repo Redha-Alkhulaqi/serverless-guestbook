f15e7feffaac28d95ac98fb367179888bff46b15
Redha-Alkhulaqi <81948469+Redha-Alkhulaqi@users.noreply.github.com>
2022-08-01 05:53:54 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://dd7d188f.us-south.apigw.appdomain.cloud/guestbook/entries';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
